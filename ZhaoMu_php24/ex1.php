e0a354287bcdaa0d30ead7bca936a59daf872d94
Mu Zhao <zhaomu817@163.com>
2024-01-26 12:58:13 +0200

    finished

---
+++ ZhaoMu_php24/ex1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Mu</title>
+</head>
+<body>
+    <h3>3.1 Write PHP code to output the following message: 
+        Hello world! My name is "David"
+    </h3>
+    <?php
+    // use backslash to escape special characters like
+    echo"Hello world! My name is \"David\"";
+    ?>
+
+    <h3>3.2 Create a PHP variable named $title and assign it 
+        the value "PHP is interesting." 
+        Then, use this variable as the content within an (heading 4) element.</h3>
+    <?php 
+        $title = "PHP is interesting.";
+        echo"<h4>$title</h4>";
+    ?>
+
+    <h3>3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables 
+        represent the grades of three students in the course. To display this 
+        information, create an HTML table within your PHP code. The table should 
+        be structured with columns for Serial Number (S.n.), Name, and Grade, 
+        and it should look like this:</h3>
+
+    <?php
+        $g1 = 5;
+        $g2 = 4;
+        $g3 = 5;
+    
+    ?>
+<table>
+    <tr><th>S.n.</th><th>Name</th><th>Grade</th></tr>
+    <tr>
+        <td>1</td>
+        <td>John</td>
+        <td><?php echo $g1 ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Alice</td>
+        <td><?php echo $g2 ?></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Bob</td>
+        <td><?php echo $g3 ?></td>
+    </tr>
+</table>
+
+<h3>Take a screenshot that confirms your development environment setup and include 
+it as an image in the "ex1.php" file.h3>
+<img src="images/24.png" alt="web development environment">
+</body>
+</html>
---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+<!-- this is printing -->
+
+<?php
+//this is printing
+echo"<h1>Hello World</h1>";
+?>
+    
+</body>
+</html>
+
+
+
